32f9d321e7dee1302ca3ff9f65b83436121b7f82
Artur Arseniev <artur.catch@hotmail.it>
2023-05-31 13:44:42 +0400

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,6 @@ export type PluginOptions = {
   position?: 'left' | 'center' | 'right';
 };
 
-const stopPropagation = e => e.stopPropagation();
-
 const plugin: Plugin<PluginOptions> = (editor, options = {}) => {
   const opts: Required<PluginOptions> = {
     options: {},
@@ -38,99 +36,6 @@ const plugin: Plugin<PluginOptions> = (editor, options = {}) => {
   if (!ck) {
     throw new Error('CKEDITOR instance not found');
   }
-
-  // editor.setCustomRte({
-  //   enable(el, rte) {
-  //     // If already exists I'll just focus on it
-  //     if(rte && rte.status != 'destroyed') {
-  //       this.focus(el, rte);
-  //     	return rte;
-  //     }
-
-  //     el.contentEditable = true;
-
-  //     // Seems like 'sharedspace' plugin doesn't work exactly as expected
-  //     // so will help hiding other toolbars already created
-  //     let rteToolbar = editor.RichTextEditor.getToolbarEl();
-  //     [].forEach.call(rteToolbar.children, (child) => {
-  //     	child.style.display = 'none';
-  //     });
-
-  //     // Check for the mandatory options
-  //     var opt = opts.options;
-  //     var plgName = 'sharedspace';
-
-  //     if (opt.extraPlugins) {
-  //       if (typeof opt.extraPlugins === 'string')
-  //         opt.extraPlugins += ',' + plgName;
-  //       else
-  //         opt.extraPlugins.push(plgName);
-  //     } else {
-  //       opt.extraPlugins = plgName;
-  //     }
-
-  //     if(!opts.options.sharedSpaces) {
-  //       opts.options.sharedSpaces = {top: rteToolbar};
-  //     }
-
-  //     // Init CkEditors
-  //     rte = CKEDITOR.inline(el, opts.options);
-
-  //     /**
-  //      * Implement the `rte.getContent` method so that GrapesJS is able to retrieve CKE's generated content (`rte.getData`) properly
-  //      *
-  //      * See:
-  //      *  - {@link https://github.com/artf/grapesjs/issues/2916}
-  //      *  - {@link https://github.com/artf/grapesjs/blob/dev/src/dom_components/view/ComponentTextView.js#L80}
-  //      *  - {@link https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_editor.html#method-getData}
-  //      */
-  //     rte.getContent = rte.getData;
-
-  //     // Make click event propogate
-  //     rte.on('contentDom', () => {
-  //       var editable = rte.editable();
-  //       editable.attachListener(editable, 'click', () => {
-  //         el.click();
-  //       });
-  //     });
-
-  //     // The toolbar is not immediatly loaded so will be wrong positioned.
-  //     // With this trick we trigger an event which updates the toolbar position
-  //     rte.on('instanceReady', e => {
-  //       var toolbar = rteToolbar.querySelector('#cke_' + rte.name);
-  //       if (toolbar) {
-  //         toolbar.style.display = 'block';
-  //       }
-  //       editor.trigger('canvasScroll')
-  //     });
-
-  //     // Prevent blur when some of CKEditor's element is clicked
-  //     rte.on('dialogShow', e => {
-  //       const editorEls = grapesjs.$('.cke_dialog_background_cover, .cke_dialog');
-  //       ['off', 'on'].forEach(m => editorEls[m]('mousedown', stopPropagation));
-  //     });
-
-  //     this.focus(el, rte);
-
-
-  //     return rte;
-  //   },
-
-  //   disable(el, rte) {
-  //     el.contentEditable = false;
-  //     if(rte && rte.focusManager)
-  //       rte.focusManager.blur(true);
-  //   },
-
-  //   focus(el, rte) {
-  //     // Do nothing if already focused
-  //     if (rte && rte.focusManager.hasFocus) {
-  //       return;
-  //     }
-  //     el.contentEditable = true;
-  //     rte && rte.focus();
-  //   },
-  // });
 
   const stopPropagation = (ev: Event) => ev.stopPropagation();
   const updateEditorToolbars = () => setTimeout(() => editor.refresh(), 0);
@@ -229,25 +134,17 @@ const plugin: Plugin<PluginOptions> = (editor, options = {}) => {
 
   // Update RTE toolbar position
   editor.on('rteToolbarPosUpdate', (pos) => {
-    // Update by position
+    // console.log('rteToolbarPosUpdate', pos)
+    const { elRect } = pos;
+
     switch (opts.position) {
       case 'center':
-        let diff = (pos.elementWidth / 2) - (pos.targetWidth / 2);
-        pos.left = pos.elementLeft + diff;
+        pos.left = (elRect.width / 2) - (pos.targetWidth / 2);
         break;
       case 'right':
-        let width = pos.targetWidth;
-        pos.left = pos.elementLeft + pos.elementWidth - width;
+        pos.left = ''
+        pos.right = 0;
         break;
-    }
-
-    if (pos.top <= pos.canvasTop) {
-      pos.top = pos.elementTop + pos.elementHeight;
-    }
-
-    // Check if not outside of the canvas
-    if (pos.left < pos.canvasLeft) {
-      pos.left = pos.canvasLeft;
     }
   });
 };
